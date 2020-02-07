71e8ccda60ff61cc3c738789d6535ab736f013d8
ounihamza100 <hamza.ellouni@gmail.com>
2020-02-07 18:29:11 +0100

    servlet cookie demo

---
+++ src/main/java/io/login/LogoutServlet.java
@@ -0,0 +1,36 @@
+package io.login;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Hamza Ouni
+ */
+@WebServlet("/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        Cookie loginCookie = null;
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null){
+            for(Cookie cookie : cookies){
+                if(cookie.getName().equals("xxx")){
+                    loginCookie = cookie;
+                    break;
+                }
+            }
+        }
+        if(loginCookie != null){
+            loginCookie.setMaxAge(0);
+            response.addCookie(loginCookie);
+        }
+        response.sendRedirect("login.html");
+    }
+
+}
